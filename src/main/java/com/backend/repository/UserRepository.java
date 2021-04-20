39906bd45734571d97648279110fa7ea3bc62715
Snowfall99 <445117445@qq.com>
2021-04-20 16:10:04 +0800

    repository level

---
+++ src/main/java/com/backend/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.backend.repository;
+
+import com.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query(value = "from User where username = :username and password = :password")
+    User checkUser(@Param("username") String username, @Param("password") String password);
+
+    @Query("select u from User u")
+    List<User> getUsers();
+
+    @Query(value = "from User where username = :username")
+    User findByName(@Param("username") String username);
+}
