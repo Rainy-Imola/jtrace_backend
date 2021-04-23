922493c4187f505ee6bb9827fe69df6e7d9f9b04
Snowfall99 <445117445@qq.com>
2021-04-23 10:40:36 +0800

    add testcases

--- src/test/java/com/backend/BackendApplicationTests.java
+++ src/test/java/com/backend/BackendApplicationTests.java
@@ -1,9 +1,40 @@
 package com.backend;
 
+import com.backend.entity.User;
+import com.backend.service.MessageService;
+import com.backend.service.PictureService;
+import com.backend.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class BackendApplicationTests {
+    @Autowired
+    MessageService messageService;
 
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    PictureService pictureService;
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void userTest() throws Exception {
+    }
+
+    @Test
+    public void messageTest() throws Exception {
+    }
+
+    @Test
+    public void pictureTest() throws Exception {
+    }
 }
