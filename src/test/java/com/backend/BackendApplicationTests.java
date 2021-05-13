02c305e32cb5eebc357c373a1f8c47daa6ef547f
Snowfall99 <445117445@qq.com>
2021-05-13 21:37:45 +0800

    message test and comment test

--- src/test/java/com/backend/BackendApplicationTests.java
+++ src/test/java/com/backend/BackendApplicationTests.java
@@ -1,13 +1,21 @@
 package com.backend;
 
+import com.backend.entity.Comment;
+import com.backend.entity.Message;
 import com.backend.entity.User;
 import com.backend.repository.jpa.UserRepository;
+import com.backend.repository.mongo.CommentRepository;
+import com.backend.repository.mongo.MessageRepository;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.CacheManager;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,10 +25,16 @@ public class BackendApplicationTests {
     private UserRepository userRepository;
 
     @Autowired
+    private MessageRepository messageRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
     private CacheManager cacheManager;
 
     @Test
-    public void test() throws Exception {
+    public void testUser() throws Exception {
         userRepository.save(new User(1, "test", "123"));
 
         User u1 = userRepository.findByName("test");
@@ -29,4 +43,26 @@ public class BackendApplicationTests {
         User u2 = userRepository.findByName("test");
         System.out.println("second check:" + u2.getPassword());
     }
+
+    @Test
+    public void testMessage() throws Exception {
+        List<Message> messages_before = messageRepository.findByAuthor(1);
+
+        messageRepository.save(new Message(1, 1, "test message1", new Date()));
+        messageRepository.save(new Message(2, 1, "test message2", new Date()));
+
+        List<Message> messages_after = messageRepository.findByAuthor(1);
+//        Assert.assertEquals(messages_before.size() + 2, messages_after.size());
+    }
+
+    @Test
+    public void testComment() throws Exception {
+        commentRepository.save(new Comment(1, 100, "test comment1", new Date(), 100));
+        commentRepository.save(new Comment(2, 100, "test comment2", new Date(), 100));
+
+        List<Comment> comments = commentRepository.findCommentsByMessage(100);
+
+        Assert.assertEquals(2, comments.size());
+    }
+
 }
