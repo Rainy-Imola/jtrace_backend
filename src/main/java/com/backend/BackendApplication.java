7cb251a15e6e4c6fde971b243d14181fe0d408b4
Snowfall99 <445117445@qq.com>
2021-05-14 08:05:46 +0800

    swagger

--- src/main/java/com/backend/BackendApplication.java
+++ src/main/java/com/backend/BackendApplication.java
@@ -1,10 +1,12 @@
 package com.backend;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
+@EnableSwagger2Doc
 @SpringBootApplication
 public class BackendApplication {
 
