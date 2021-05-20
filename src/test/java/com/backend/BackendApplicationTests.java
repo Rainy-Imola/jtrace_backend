8818a486021aa566f4a6986fd1d61a12af66783d
Snowfall99 <445117445@qq.com>
2021-05-20 10:31:34 +0800

    unit test of repository

--- src/test/java/com/backend/BackendApplicationTests.java
+++ src/test/java/com/backend/BackendApplicationTests.java
@@ -1,9 +1,11 @@
 package com.backend;
 
+import com.backend.entity.Chat;
 import com.backend.entity.Comment;
 import com.backend.entity.Message;
 import com.backend.entity.User;
 import com.backend.repository.jpa.UserRepository;
+import com.backend.repository.mongo.ChatRepository;
 import com.backend.repository.mongo.CommentRepository;
 import com.backend.repository.mongo.MessageRepository;
 import org.junit.Assert;
@@ -31,39 +33,86 @@ public class BackendApplicationTests {
     private CommentRepository commentRepository;
 
     @Autowired
+    private ChatRepository chatRepository;
+
+    @Autowired
     private CacheManager cacheManager;
 
     @Test
-    public void testUser() throws Exception {
+    public void testUserRepo() throws Exception {
         System.out.println("CacheManager type: " + cacheManager.getClass());
 
-        userRepository.save(new User(1, "test", "123"));
+        // test getUsers
+        List<User> users_before = userRepository.getUsers();
+        userRepository.save(new User(4, "test user1", "123"));
+        userRepository.save(new User(5, "test user2", "123"));
+        List<User> users_after = userRepository.getUsers();
+        Assert.assertEquals(users_before.size() + 2, users_after.size());
 
-        User u1 = userRepository.findByName("test");
-        System.out.println("first check:" + u1.getPassword());
+        // test checkUser
+        User user1 = userRepository.checkUser("test user1", "123");
+        Assert.assertNotNull(user1);
+        User user2 = userRepository.checkUser("test checkUser", "password");
+        Assert.assertNull(user2);
 
-        User u2 = userRepository.findByName("test");
-        System.out.println("second check:" + u2.getPassword());
+        // test findByName
+        User user3 = userRepository.findByName("test user1");
+        Assert.assertNotNull(user3);
+        User user4 = userRepository.findByName("test checkByName");
+        Assert.assertNull(user4);
+
+        // test findByEmail
+        String email = "12345@sjtu.edu.cn";
+        user3.setEmail(email);
+        userRepository.save(user3);
+        User user5 = userRepository.findByEmail(email);
+        Assert.assertNotNull(user5);
+        User user6 = userRepository.findByEmail("123@sjtu.edu.cn");
+        Assert.assertNull(user6);
+
+        userRepository.delete(userRepository.findByName("test user1"));
+        userRepository.delete(userRepository.findByName("test user2"));
     }
 
     @Test
-    public void testMessage() throws Exception {
+    public void testMessageRepo() throws Exception {
+        // test findByAuthor
         List<Message> messages_before = messageRepository.findByAuthor(1);
-
         messageRepository.save(new Message(1, 1, "test message1", new Date(), 0));
         messageRepository.save(new Message(2, 1, "test message2", new Date(), 0));
-
         List<Message> messages_after = messageRepository.findByAuthor(1);
+
+        Assert.assertEquals(messages_before.size() + 2, messages_after.size());
+
+        messageRepository.delete(messageRepository.findById(1).get());
+        messageRepository.delete(messageRepository.findById(2).get());
     }
 
     @Test
-    public void testComment() throws Exception {
-        commentRepository.save(new Comment(1, 100, "test comment1", new Date(), 100));
-        commentRepository.save(new Comment(2, 100, "test comment2", new Date(), 100));
+    public void testCommentRepo() throws Exception {
+        // test findCommentByMessage
+        List<Comment> comments_before = commentRepository.findCommentsByMessage(100);
+        Comment comment1 = new Comment(1, 100, "test comment1", new Date(), 100);
+        Comment comment2 = new Comment(2, 100, "test comment1", new Date(), 100);
+        commentRepository.save(comment1);
+        commentRepository.save(comment2);
+        List<Comment> comments_after = commentRepository.findCommentsByMessage(100);
+        Assert.assertEquals(comments_before.size() + 2, comments_after.size());
 
-        List<Comment> comments = commentRepository.findCommentsByMessage(100);
+        commentRepository.delete(comment1);
+        commentRepository.delete(comment2);
+    }
 
-        Assert.assertEquals(2, comments.size());
+    @Test
+    public void testChatRepo() throws Exception {
+        // test getChatByFromAndTo
+        List<Chat> chats_before = chatRepository.getChatsByFromAndTo("test chatFrom", "test chatTo");
+        Chat chat1 = new Chat(1, "test chatFrom", "test chatTo", "test getChatByFromAndTo", "2021-5-20");
+        chatRepository.save(chat1);
+        List<Chat> chats_after = chatRepository.getChatsByFromAndTo("test chatFrom", "test chatTo");
+        Assert.assertEquals(chats_before.size() + 1, chats_after.size());
+
+        chatRepository.delete(chat1);
     }
 
 }
